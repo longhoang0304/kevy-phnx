ad556dca1292afce77426bf619df58d606ef478a
Long Hoang <longhoang0304@users.noreply.github.com>
2024-01-17 19:21:27 +0000

    Renamed CommandExecutor to CommandExecutorError

--- src/execution-engines/commands/unknown.rs
+++ src/execution-engines/commands/unknown.rs
@@ -1,13 +1,13 @@
 use std::error::Error;
 
-use crate::exe_engine::cores::{Command, CommandError, CommandExecutor, CommandResult};
+use crate::exe_engine::cores::{Command, CommandExecutorError, CommandExecutor, CommandResult};
 use crate::storage::cores::Storage;
 
 pub struct Unknown;
 
 impl CommandExecutor for Unknown {
     fn execute(_: &mut Box<dyn Storage>, cmd: &Command) -> Result<CommandResult, Box<dyn Error>> {
-        let err = Box::new(CommandError::UnknownCommand(String::from(cmd.name)));
+        let err = Box::new(CommandExecutorError::UnknownCommand(String::from(cmd.name)));
         Err(err)
     }
 }
--- src/execution-engines/cores/mod.rs
+++ src/execution-engines/cores/mod.rs
@@ -1,5 +1,5 @@
 pub use command::*;
-pub use command_errors::*;
+pub use command_executor_errors::*;
 pub use command_executor::*;
 pub use command_parameter::*;
 pub use command_result::*;
@@ -10,6 +10,6 @@ mod command;
 mod command_executor;
 mod command_parameter;
 mod command_result;
-mod command_errors;
+mod command_executor_errors;
 mod engine_errors;
 mod engine;
