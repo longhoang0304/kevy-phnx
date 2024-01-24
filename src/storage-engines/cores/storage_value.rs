a65dcae2dd421c50d21a1cb48758ba18153fff8f
Long Hoang <longhoang0304@users.noreply.github.com>
2024-01-24 16:57:21 +0000

    fixed: expired value should return nil

--- src/storage-engines/cores/storage_value.rs
+++ src/storage-engines/cores/storage_value.rs
@@ -1,16 +1,34 @@
+use std::time::SystemTime;
+
 use super::StorageData;
 
 #[derive(Debug, Clone)]
 pub struct StorageValue {
     pub data: StorageData,
-    pub ttl: Option<i128>,
+    pub expire_at: Option<i128>,
 }
 
 impl StorageValue {
-    pub fn new(data: StorageData, ttl: Option<i128>) -> StorageValue {
+    pub fn new(data: StorageData, expire_at: Option<i128>) -> StorageValue {
         StorageValue {
             data,
-            ttl,
+            expire_at,
         }
+    }
+
+    pub fn is_expired(&self) -> bool {
+        let expire_at = &self.expire_at;
+        if expire_at.is_none() {
+            return false;
+        }
+
+        let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis() as i128;
+        let expire_at = expire_at.unwrap();
+
+        if expire_at <= now {
+            return true;
+        }
+
+        false
     }
 }
